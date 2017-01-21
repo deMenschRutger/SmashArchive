24efb86039a29ff8ca3f3510c18eadd45d358d2b
Rutger Mensch <rutgermensch@gmail.com>
2017-01-21 15:28:49 +0100

    Initial Symfony set-up

---
+++ deploy/src/AppBundle/Command/TournamentImportCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace AppBundle\Command;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * @author Rutger Mensch (rutger@rutgermensch.com)
+ */
+class TournamentImportCommand extends ContainerAwareCommand
+{
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var SymfonyStyle
+     */
+    protected $io;
+
+    /**
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->entityManager = $entityManager;
+
+        parent::__construct();
+    }
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this
+            ->setName('tournament:import')
+            ->setDescription('Import a tournament from a third-party (like smash.gg)')
+        ;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->io = new SymfonyStyle($input, $output);
+        $this->io->title('Import tournament...');
+    }
+}
