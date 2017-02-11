13ad12f1d2d9d725ed42bddd365981077ce226ae
Rutger Mensch <rutgermensch@gmail.com>
2017-02-11 16:27:55 +0100

    Move part of the SmashRanking import script to a dedicated import class

--- deploy/src/AppBundle/Command/SmashRankingImportCommand.php
+++ deploy/src/AppBundle/Command/SmashRankingImportCommand.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace AppBundle\Command;
 
-use CoreBundle\Entity\Entrant;
-use CoreBundle\Entity\Event;
-use CoreBundle\Entity\Phase;
-use CoreBundle\Entity\PhaseGroup;
-use CoreBundle\Entity\Player;
-use CoreBundle\Entity\Set;
-use CoreBundle\Entity\Tournament;
+use AppBundle\Importer\SmashRanking\SmashRankingImporter;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -33,16 +27,6 @@ class SmashRankingImportCommand extends ContainerAwareCommand
      * @var SymfonyStyle
      */
     protected $io;
-
-    /**
-     * @var array
-     */
-    protected $entrants;
-
-    /**
-     * @var array
-     */
-    protected $players;
 
     /**
      * @param EntityManager $entityManager
@@ -94,12 +78,14 @@ class SmashRankingImportCommand extends ContainerAwareCommand
     }
 
     /**
+     * This method will turn the entire export of the SmashRanking database into more manageable chunks.
+     *
      * @return void
      */
     protected function categorizeModels()
     {
         $rootDir = $this->getContainer()->get('kernel')->getRootDir();
-        $jsonPath = realpath($rootDir.'/../var/tmp/db.json');
+        $jsonPath = realpath($rootDir.'/../var/tmp/smashranking/db.json');
         $json = file_get_contents($jsonPath);
         $rows = \GuzzleHttp\json_decode($json, true);
 
@@ -120,280 +106,32 @@ class SmashRankingImportCommand extends ContainerAwareCommand
             ksort($rows);
 
             $contents = \GuzzleHttp\json_encode($rows, JSON_PRETTY_PRINT);
-            $dirPath = realpath($rootDir.'/../var/tmp/');
-            $filePath = $dirPath."/sreu.{$key}.json";
+            $dirPath = realpath($rootDir.'/../var/tmp/smashranking');
+            $filePath = $dirPath."/{$key}.json";
 
             file_put_contents($filePath, $contents);
         }
     }
 
     /**
-     *
+     * @return void
      */
     protected function import()
     {
-        /*
-        eventtypes are TYPE_CHOICES = (
-        (1, u'Swiss System'),
-        (2, u'Round Robin Pool'),
-        (3, u'Bracket Pool'),
-        (4, u'Bracket'),
-        (5, u'Intermediate Bracket'),
-        (6, u'Amateur Bracket'),
-        */
+        $rootDir = $this->getContainer()->get('kernel')->getRootDir();
+        $contentDirPath = realpath($rootDir.'/../var/tmp/smashranking/');
 
-
-
-
-    /*
-    match rounds are ROUND_CHOICES = (
-        (1, u'W1'),
-        (2, u'W2'),
-        (3, u'W3'),
-        (4, u'W4'),
-        (5, u'W5'),
-        (6, u'W6'),
-        (7, u'W7'),
-        (8, u'W8'),
-        (9, u'L1'),
-        (10, u'L2'),
-        (11, u'L3'),
-        (12, u'L4'),
-        (13, u'L5'),
-        (14, u'L6'),
-        (15, u'L7'),
-        (16, u'L8'),
-        (17, u'L9'),
-        (18, u'L10'),
-        (19, u'L11'),
-        (20, u'L12'),
-        (21, u'L13'),
-        (22, u'L14'),
-        (23, u'R1'),
-        (24, u'R2'),
-        (25, u'R3'),
-        (26, u'R4'),
-        (27, u'R5'),
-        (28, u'R6'),
-        (29, u'R7'),
-        (30, u'R8'),
-        (31, u'R9'),
-        (32, u'R10'),
-        (33, u'GF1'),
-        (34, u'GF2'),
-    )
-    */
-
-
-//        $this->io->title('Import data from the smashranking.eu database...');
-
-
-
-
-        /*
-         * 1 Tournament has phases or no phases?
-         * 1.1 Phases -> Can be imported (count: 190)
-         * 1.2 No Phases -> 2 (count: 1341)
-         *
-         * 2 No phases: Single event or multiple events?
-         * 2.1 Single event -> 3 (count: 1068)
-         * 2.2 Multiple events -> 4 (count: 273)
-         *
-         * 3 Single event: is it a bracket (type 4, 5 or 6)?
-         * 3.1 Yes -> Import them all (count: 1057)
-         * 3.2 No -> Possible, but placings need to be determined in a different way (count: 11)
-         *
-         * 4 Multiple events: are they all brackets (type 4, 5 or 6)?
-         * 4.1 Yes -> They must be separate events somehow (maybe singles and doubles)?
-         * 4.2 No -> We probably have phases that weren't marked as phases.
-         */
-
-
-
-        $tournaments = $this->getFromJson('tournament');
-
-        foreach ($tournaments as $tournamentId => &$tournament) {
-            $entity = new Tournament();
-            $entity->setName($tournament['name']);
-
-            $this->entityManager->persist($entity);
-
-            $tournament = $entity;
-        }
-
-
-
-//        $smashggIds = $this->getSmashggIds();
-//
-//        $events = array_filter($this->getFromJson('event'), function ($event) use ($smashggIds) {
-//            return in_array($event['tournament'], $smashggIds);
-//        });
-
-
-
-        $events = $this->getFromJson('event');
-        $eventsPerTournament = [];
-
-        foreach ($events as $eventId => $event) {
-            $tournamentId = $event['tournament'];
-
-            if (!array_key_exists($tournamentId, $eventsPerTournament)) {
-                $eventsPerTournament[$tournamentId] = [
-                    'events' => [],
-                    'hasPhases' => false,
-                ];
-            }
-
-            $eventsPerTournament[$tournamentId]['events'][$eventId] = $event;
-
-            if ($event['phase'] !== null) {
-                $eventsPerTournament[$tournamentId]['hasPhases'] = true;
-            }
-        }
-
-        $eventsPerTournament = array_filter($eventsPerTournament, function ($tournament) {
-            return !$tournament['hasPhases'];
-        });
-
-        $eventsPerTournament = array_filter($eventsPerTournament, function ($tournament) {
-            if (count($tournament['events']) > 1) {
-                return false;
-            }
-
-            foreach ($tournament['events'] as $eventId => $event) {
-                if (!in_array($event['type'], [4, 5, 6])) {
-                    return false;
-                }
-            }
-
-            return true;
-        });
-
-        $melee = $this->entityManager->find('CoreBundle:Game', 1);
-        $phaseGroups = [];
-
-        foreach ($eventsPerTournament as $tournamentId => $tournament) {
-            $tournamentEntity = $tournaments[$tournamentId];
-
-            foreach ($tournament['events'] as $eventId => $event) {
-                $entity = new Event();
-                $entity->setName('Melee Singles');
-                $entity->setGame($melee);
-                $entity->setTournament($tournamentEntity);
-
-                $this->entityManager->persist($entity);
-
-                $phase = new Phase();
-                // TODO Change the name based on event type 4, 5 or 6.
-                $phase->setName('Bracket');
-                $phase->setEvent($entity);
-                $phase->setPhaseOrder(1);
-
-                $this->entityManager->persist($phase);
-
-                $phaseGroup = new PhaseGroup();
-                $phaseGroup->setName('Bracket');
-                // TODO Make the type an entity?
-                $phaseGroup->setType(2);
-                $phaseGroup->setPhase($phase);
-
-                $this->entityManager->persist($phaseGroup);
-
-                $phaseGroups[$eventId] = $phaseGroup;
-            }
-        }
-
-        $this->entityManager->flush();
-
-
-
-        $matches = $this->getFromJson('match');
-        $this->players = $this->getFromJson('smasher');
-        $counter = 0;
-
-        foreach ($matches as $matchId => $match) {
-            $eventId = $match['event'];
-
-            if (!array_key_exists($eventId, $phaseGroups)) {
-                continue;
-            }
-
-            /** @var PhaseGroup $phaseGroup */
-            $phaseGroup = $phaseGroups[$eventId];
-            $tournamentId = $phaseGroup->getPhase()->getEvent()->getTournament()->getId();
-            $entrantOne = $this->getEntrant($match['winner'], $tournamentId);
-            $entrantTwo = $this->getEntrant($match['loser'], $tournamentId);
-
-            $set = new Set();
-            $set->setPhaseGroup($phaseGroup);
-            // TODO Map rounds to the way smash.gg uses them.
-            $set->setRound($match['round']);
-            $set->setEntrantOne($entrantOne);
-            $set->setEntrantTwo($entrantTwo);
-            $set->setWinner($entrantOne);
-            $set->setLoser($entrantTwo);
-
-            $this->entityManager->persist($set);
-
-            $counter++;
-        }
-
-        $this->io->comment("Counted {$counter} sets.");
-
-        $this->entityManager->flush();
-
-
-
-//        $this->io->success('Successfully imported the data!');
-    }
-
-    /**
-     * @param integer $playerId
-     * @param integer $tournamentId
-     * @return Entrant|bool
-     */
-    protected function getEntrant($playerId, $tournamentId)
-    {
-        if (isset($this->entrants[$tournamentId][$playerId])) {
-            return $this->entrants[$tournamentId][$playerId];
-        }
-
-        if (!array_key_exists($playerId, $this->players)) {
-            return false;
-        }
-
-        if (array_key_exists('entity', $this->players[$playerId])) {
-            $player = $this->players[$playerId]['entity'];
-        } else {
-            $tag = $this->players[$playerId]['tag'];
-            $player = new Player();
-            $player->setGamerTag($tag);
-
-            $this->entityManager->persist($player);
-
-            $this->players[$playerId]['entity'] = $player;
-        }
-
-        $entrant = new Entrant();
-        $entrant->setName($player->getGamerTag());
-        $entrant->addPlayer($player);
-        $player->addEntrant($entrant);
-
-        $this->entityManager->persist($entrant);
-
-        $this->entrants[$tournamentId][$playerId] = $entrant;
-
-        return $entrant;
+        $importer = new SmashRankingImporter($contentDirPath, $this->io, $this->entityManager);
+        $importer->import();
     }
 
     /**
      * @return array
+     * @deprecated
      */
     protected function getSmashggIds()
     {
-//        $this->io->title('Import smash.gg data from the smashranking.eu database...');
-
-        $tournaments = array_filter($this->getFromJson('tournament'), function ($tournament) {
+        $tournaments = array_filter($this->getContentFromJson('tournament'), function ($tournament) {
             if (array_key_exists('smashgg_page', $tournament) &&
                 mb_strlen($tournament['smashgg_page']) > 0
             ) {
@@ -409,25 +147,6 @@ class SmashRankingImportCommand extends ContainerAwareCommand
             return false;
         });
 
-//        $this->io->success('Successfully imported the data!');
-
         return array_keys($tournaments);
-    }
-
-    /**
-     * @param string $key
-     * @return array
-     */
-    protected function getFromJson(string $key)
-    {
-        $rootDir = $this->getContainer()->get('kernel')->getRootDir();
-        $jsonPath = realpath($rootDir."/../var/tmp/sreu.ranking.{$key}.json");
-        $json = file_get_contents($jsonPath);
-
-        if (!$json) {
-            throw new \InvalidArgumentException("No JSON file found for key {$key}.");
-        }
-
-        return \GuzzleHttp\json_decode($json, true);
     }
 }
---
+++ deploy/src/AppBundle/Importer/SmashRanking/SmashRankingImporter.php
@@ -0,0 +1,365 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AppBundle\Importer\SmashRanking;
+
+use CoreBundle\Entity\Entrant;
+use CoreBundle\Entity\Event;
+use CoreBundle\Entity\Game;
+use CoreBundle\Entity\Phase;
+use CoreBundle\Entity\PhaseGroup;
+use CoreBundle\Entity\Player;
+use CoreBundle\Entity\Set;
+use CoreBundle\Entity\Tournament;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+/**
+ * @author Rutger Mensch <rutger@rutgermensch.com>
+ *
+ * @TODO Take into account 'forfeit' and 'publish' on the match model.
+ */
+class SmashRankingImporter
+{
+    /**
+     * @var array
+     */
+    protected $eventTypes = [
+        1 => 'Swiss System',
+        2 => 'Round Robin Pool',
+        3 => 'Bracket Pool',
+        4 => 'Bracket',
+        5 => 'Intermediate Bracket',
+        6 => 'Amateur Bracket',
+    ];
+
+    /**
+     * @var array
+     */
+    protected $rounds = [
+        1 => 1, // 'W1'
+        2 => 2, // 'W2'
+        3 => 3, // 'W3'
+        4 => 4, // 'W4'
+        5 => 5, // 'W5'
+        6 => 6, // 'W6'
+        7 => 7, // 'W7'
+        8 => 8, // 'W8'
+        9 => 9, // 'L1'
+        10 => -1, // 'L2'
+        11 => -2, // 'L3'
+        12 => -3, // 'L4'
+        13 => -4, // 'L5'
+        14 => -5, // 'L6'
+        15 => -6, // 'L7'
+        16 => -7, // 'L8'
+        17 => -8, // 'L9'
+        18 => -9, // 'L10'
+        19 => -10, // 'L11'
+        20 => -11, // 'L12'
+        21 => -12, // 'L13'
+        22 => -13, // 'L14'
+        23 => -14, // 'R1'
+        24 => -15, // 'R2'
+        25 => -16, // 'R3'
+        26 => 'R4',
+        27 => 'R5',
+        28 => 'R6',
+        29 => 'R7',
+        30 => 'R8',
+        31 => 'R9',
+        32 => 'R10',
+        33 => 10, // 'GF1'
+        34 => 11, // 'GF2'
+    ];
+
+    /**
+     * @var string
+     */
+    protected $contentDirPath;
+
+    /**
+     * @var SymfonyStyle
+     */
+    protected $io;
+
+    /**
+     * @var EntityManager
+     */
+    protected $entityManager;
+
+    /**
+     * @var Game
+     */
+    protected $melee;
+
+    /**
+     * @var array
+     */
+    protected $entrants;
+
+    /**
+     * @var array
+     */
+    protected $players;
+
+    /**
+     * @param string        $contentDirPath
+     * @param SymfonyStyle  $io
+     * @param EntityManager $entityManager
+     */
+    public function __construct(string $contentDirPath, SymfonyStyle $io, EntityManager $entityManager)
+    {
+        $this->contentDirPath = $contentDirPath;
+        $this->io = $io;
+        $this->entityManager = $entityManager;
+
+        $this->melee = $this->entityManager->find('CoreBundle:Game', 1);
+    }
+
+    /**
+     * @return void
+     */
+    public function import()
+    {
+        /*
+         * 1 Tournament has phases or no phases?
+         * 1.1 Phases -> Can be imported (count: 190)
+         * 1.2 No Phases -> 2 (count: 1341)
+         *
+         * 2 No phases: Single event or multiple events?
+         * 2.1 Single event -> 3 (count: 1068)
+         * 2.2 Multiple events -> 4 (count: 273)
+         *
+         * 3 Single event: is it a bracket (type 4, 5 or 6)?
+         * 3.1 Yes -> Import them all (count: 1057)
+         * 3.2 No -> Possible, but placings need to be determined in a different way (count: 11)
+         *
+         * 4 Multiple events: are they all brackets (type 4, 5 or 6)?
+         * 4.1 Yes -> They must be separate events somehow (maybe singles and doubles)?
+         * 4.2 No -> We probably have phases that weren't marked as phases.
+         */
+
+        $this->io->title('Import data from the smashranking.eu database...');
+
+        $this->io->text('Importing tournaments...');
+        $tournaments = $this->getTournaments();
+
+        $this->io->text('Importing events...');
+        $events = $this->getEvents();
+
+        $this->io->text('Processing events...');
+        $phaseGroups = $this->processEvents($events, $tournaments);
+
+        $this->io->text('Flushing entity manager...');
+        $this->entityManager->flush();
+
+        $this->io->text('Processing phase groups...');
+        $this->processPhaseGroups($phaseGroups);
+
+        $this->entityManager->flush();
+        $this->io->success('Successfully imported the data from smashranking.eu!');
+    }
+
+    /**
+     * @param string $contentKey
+     * @return array
+     */
+    protected function getContentFromJson(string $contentKey)
+    {
+        $jsonPath = realpath($this->contentDirPath."/ranking.{$contentKey}.json");
+        $json = file_get_contents($jsonPath);
+
+        if (!$json) {
+            throw new \InvalidArgumentException("No JSON file found for key {$contentKey}.");
+        }
+
+        return \GuzzleHttp\json_decode($json, true);
+    }
+
+    /**
+     * @return array
+     */
+    protected function getTournaments()
+    {
+        $tournaments = $this->getContentFromJson('tournament');
+
+        foreach ($tournaments as $tournamentId => &$tournament) {
+            $entity = new Tournament();
+            $entity->setName($tournament['name']);
+
+            $this->entityManager->persist($entity);
+
+            $tournament = $entity;
+        }
+
+        return $tournaments;
+    }
+
+    /**
+     * @return array
+     */
+    protected function getEvents()
+    {
+        $events = $this->getContentFromJson('event');
+        $eventsPerTournament = [];
+
+        foreach ($events as $eventId => $event) {
+            $tournamentId = $event['tournament'];
+
+            if (!array_key_exists($tournamentId, $eventsPerTournament)) {
+                $eventsPerTournament[$tournamentId] = [
+                    'events' => [],
+                    'hasPhases' => false,
+                ];
+            }
+
+            $eventsPerTournament[$tournamentId]['events'][$eventId] = $event;
+
+            if ($event['phase'] !== null) {
+                $eventsPerTournament[$tournamentId]['hasPhases'] = true;
+            }
+        }
+
+        $eventsPerTournament = array_filter($eventsPerTournament, function ($tournament) {
+            return !$tournament['hasPhases'];
+        });
+
+        $eventsPerTournament = array_filter($eventsPerTournament, function ($tournament) {
+            if (count($tournament['events']) > 1) {
+                return false;
+            }
+
+            foreach ($tournament['events'] as $eventId => $event) {
+                if (!in_array($event['type'], [4, 5, 6])) {
+                    return false;
+                }
+            }
+
+            return true;
+        });
+
+        return $eventsPerTournament;
+    }
+
+    /**
+     * @param array $events
+     * @param array $tournaments
+     * @return array
+     */
+    protected function processEvents(array $events, array $tournaments)
+    {
+        $phaseGroups = [];
+
+        foreach ($events as $tournamentId => $tournament) {
+            $tournamentEntity = $tournaments[$tournamentId];
+
+            foreach ($tournament['events'] as $eventId => $event) {
+                $entity = new Event();
+                $entity->setName('Melee Singles');
+                $entity->setGame($this->melee);
+                $entity->setTournament($tournamentEntity);
+
+                $this->entityManager->persist($entity);
+
+                $phaseName = $this->eventTypes[$event['type']];
+                $phase = new Phase();
+                $phase->setName($phaseName);
+                $phase->setEvent($entity);
+                $phase->setPhaseOrder(1);
+
+                $this->entityManager->persist($phase);
+
+                $phaseGroup = new PhaseGroup();
+                $phaseGroup->setName('Bracket');
+                $phaseGroup->setType(2);
+                $phaseGroup->setPhase($phase);
+
+                $this->entityManager->persist($phaseGroup);
+
+                $phaseGroups[$eventId] = $phaseGroup;
+            }
+        }
+
+        return $phaseGroups;
+    }
+
+    /**
+     * @param array $phaseGroups
+     */
+    protected function processPhaseGroups(array $phaseGroups)
+    {
+        $matches = $this->getContentFromJson('match');
+        $this->players = $this->getContentFromJson('smasher');
+        $counter = 0;
+
+        foreach ($matches as $matchId => $match) {
+            $eventId = $match['event'];
+
+            if (!array_key_exists($eventId, $phaseGroups)) {
+                continue;
+            }
+
+            /** @var PhaseGroup $phaseGroup */
+            $phaseGroup = $phaseGroups[$eventId];
+            $tournamentId = $phaseGroup->getPhase()->getEvent()->getTournament()->getId();
+            $entrantOne = $this->getEntrant($match['winner'], $tournamentId);
+            $entrantTwo = $this->getEntrant($match['loser'], $tournamentId);
+
+            $set = new Set();
+            $set->setPhaseGroup($phaseGroup);
+            // TODO Map rounds to the way smash.gg uses them.
+            $set->setRound($match['round']);
+            $set->setEntrantOne($entrantOne);
+            $set->setEntrantTwo($entrantTwo);
+            $set->setWinner($entrantOne);
+            $set->setLoser($entrantTwo);
+
+            $this->entityManager->persist($set);
+
+            $counter++;
+        }
+
+        $this->io->comment("Counted {$counter} sets.");
+    }
+
+    /**
+     * @param integer $playerId
+     * @param integer $tournamentId
+     * @return Entrant|bool
+     */
+    protected function getEntrant($playerId, $tournamentId)
+    {
+        if (isset($this->entrants[$tournamentId][$playerId])) {
+            return $this->entrants[$tournamentId][$playerId];
+        }
+
+        if (!array_key_exists($playerId, $this->players)) {
+            return false;
+        }
+
+        if (array_key_exists('entity', $this->players[$playerId])) {
+            $player = $this->players[$playerId]['entity'];
+        } else {
+            $tag = $this->players[$playerId]['tag'];
+            $player = new Player();
+            $player->setGamerTag($tag);
+
+            $this->entityManager->persist($player);
+
+            $this->players[$playerId]['entity'] = $player;
+        }
+
+        $entrant = new Entrant();
+        $entrant->setName($player->getGamerTag());
+        $entrant->addPlayer($player);
+        $player->addEntrant($entrant);
+
+        $this->entityManager->persist($entrant);
+
+        $this->entrants[$tournamentId][$playerId] = $entrant;
+
+        return $entrant;
+    }
+}
