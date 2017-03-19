26318f5e995446da565717abf4d03cc1e5aa8b5a
Rutger Mensch <rutgermensch@gmail.com>
2017-03-19 15:44:30 +0100

    Refactor the bracket classes

--- deploy/src/CoreBundle/Bracket/AbstractBracket.php
+++ deploy/src/CoreBundle/Bracket/AbstractBracket.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace CoreBundle\Bracket;
 
+use CoreBundle\Entity\Entrant;
 use CoreBundle\Entity\PhaseGroup;
+use CoreBundle\Entity\Set;
 
 /**
  * @author Rutger Mensch <rutger@rutgermensch.com>
@@ -17,17 +19,71 @@ abstract class AbstractBracket
     protected $phaseGroup;
 
     /**
+     * @var array
+     */
+    protected $setsByRound = [];
+
+    /**
+     * @var array
+     */
+    protected $entrants = [];
+
+    /**
      * @param PhaseGroup $phaseGroup
      */
     public function __construct(PhaseGroup $phaseGroup)
     {
         $this->phaseGroup = $phaseGroup;
 
-        $this->init();
+        /** @var Set $set */
+        foreach ($this->phaseGroup->getSets() as $set) {
+            $this->addEntrant($set->getEntrantOne());
+            $this->addEntrant($set->getEntrantTwo());
+
+            $round = $set->getRound();
+            $this->setsByRound[$round][] = $set;
+        }
+
+        // Make sure the rounds are in the right order.
+        ksort($this->setsByRound);
+
+        // Reset the indexes in case certain round numbers were skipped for some reason.
+        $this->setsByRound = array_values($this->setsByRound);
     }
 
     /**
-     * @return void
+     * @param Entrant $entrant
      */
-    abstract protected function init();
+    protected function addEntrant($entrant)
+    {
+        if (!$entrant instanceof Entrant) {
+            return;
+        }
+
+        if (!in_array($entrant, $this->entrants, true)) {
+            $this->entrants[] = $entrant;
+        }
+    }
+
+    /**
+     * @param Set $set
+     * @return int
+     */
+    protected function getReverseIndex(Set $set)
+    {
+        $entrantCount = count($this->entrants);
+        $totalRounds = ceil(log($entrantCount, 2));
+
+        return intval($totalRounds) - $set->getRound();
+    }
+
+    /**
+     * @param Set $set
+     */
+    abstract protected function determineRoundName(Set $set);
+
+    /**
+     * @param Set $set
+     */
+    abstract protected function determineIsGrandFinals(Set $set);
 }
--- deploy/src/CoreBundle/Bracket/SingleEliminationBracket.php
+++ deploy/src/CoreBundle/Bracket/SingleEliminationBracket.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace CoreBundle\Bracket;
 
-use CoreBundle\Entity\Entrant;
 use CoreBundle\Entity\Set;
 
 /**
@@ -13,28 +12,11 @@ use CoreBundle\Entity\Set;
 class SingleEliminationBracket extends AbstractBracket
 {
     /**
-     * @var array
-     */
-    protected $setsById = [];
-
-    /**
-     * @var array
-     */
-    protected $setsByRound = [];
-
-    /**
-     * @var array
-     */
-    protected $entrants = [];
-
-    /**
      * @param Set $set
      */
     public function determineRoundName(Set $set)
     {
-        $entrantCount = count($this->entrants);
-        $totalRounds = ceil(log($entrantCount, 2));
-        $reverseIndex = intval($totalRounds) - $set->getRound();
+        $reverseIndex = $this->getReverseIndex($set);
 
         switch ($reverseIndex) {
             case 0:
@@ -59,50 +41,8 @@ class SingleEliminationBracket extends AbstractBracket
      */
     public function determineIsGrandFinals(Set $set)
     {
-        $entrantCount = count($this->entrants);
-        $totalRounds = ceil(log($entrantCount, 2));
-        $reverseIndex = intval($totalRounds) - $set->getRound();
-
-        if ($reverseIndex === 0) {
+        if ($this->getReverseIndex($set) === 0) {
             $set->setIsGrandFinals(true);
-        }
-    }
-
-    /**
-     * @return void
-     */
-    protected function init()
-    {
-        /** @var Set $set */
-        foreach ($this->phaseGroup->getSets() as $set) {
-            $setId = $set->getId();
-            $round = $set->getRound();
-
-            $this->addEntrant($set->getEntrantOne());
-            $this->addEntrant($set->getEntrantTwo());
-
-            $this->setsById[$setId] = $set;
-            $this->setsByRound[$round][] = $set;
-        }
-
-        // Make sure the rounds are in the right order.
-        ksort($this->setsByRound);
-
-        // Reset the indexes in case certain round numbers were skipped for some reason.
-        $this->setsByRound = array_values($this->setsByRound);
-    }
-
-    /**
-     * @param Entrant $entrant
-     */
-    protected function addEntrant($entrant)
-    {
-        if (!$entrant instanceof Entrant) {
-            return;
-        }
-
-        if (!in_array($entrant, $this->entrants, true)) {
-            $this->entrants[] = $entrant;
         }
     }
 }
