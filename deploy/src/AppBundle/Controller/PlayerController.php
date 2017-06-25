64e3a568aec95b0431d24914377bc9c043ee39c8
Rutger Mensch <rutgermensch@gmail.com>
2017-06-25 18:27:37 +0200

    Add stopwatch middleware for the command bus

--- deploy/src/AppBundle/Controller/PlayerController.php
+++ deploy/src/AppBundle/Controller/PlayerController.php
@@ -13,6 +13,7 @@ use Domain\Command\Player\SetsCommand;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Stopwatch\Stopwatch;
 
 /**
  * @author Rutger Mensch <rutger@rutgermensch.com>
